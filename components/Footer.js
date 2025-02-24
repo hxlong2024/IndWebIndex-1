f4e572902abbcc95f1843241a8f3ad5e35039f7b
hxlong2024 <hxlong@zufe.edu.cn>
2025-02-24 16:02:11 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -19,28 +19,31 @@ const Footer = () => {
 
   return (
 <div className='text-xs whitespace-nowrap text-purple-900 dark:text-rose-200 py-4'>
-    <div className='flex space-x-4'>
-        <a
-            href='https://www.notion.so/la4ee4c653c080beb580f0fbbb905be3?v=la4ee4c653c08147b7b00000c9784bf6b'
-            target="_blank"
-            rel='noopener noreferrer'
-            className='underline'
-        >
-            增加网页
-        </a>
-        <a
-            href='https://github.com/hxlong2024/IndWebIndex-1/blob/main/README.md'
-            target="_blank"
-            rel='noopener noreferrer'
-            className='underline'
-        >
-            获取更新
-        </a>
+<div className='flex items-center justify-center min-h-screen text-xs whitespace-nowrap text-purple-900 dark:text-rose-200 py-4'>
+    <div className='text-center'>
+        <div className='flex space-x-4 justify-center'>
+            <a
+                href='https://www.notion.so/1a4ee4c653c080beb580f0f0bb905be3?v=1a4ee4c653c08147b7b0000c9784bf6b'
+                target="_blank"
+                rel='noopener noreferrer'
+                className='underline'
+            >
+                增加网页
+            </a>
+            <a
+                href='https://github.com/hxlong2024/IndWebIndex-1/blob/main/README.md'
+                target="_blank"
+                rel='noopener noreferrer'
+                className='underline'
+            >
+                获取更新
+            </a>
+        </div>
+        <p className='mb-1'>
+            {visitCount !== null ? `访问量：${visitCount}` : '访问量：加载中...'}
+        </p>
+        <p>Copyright © 2021 - 2025 Linfong</p>
     </div>
-    <p className='mb-1'>
-        {visitCount !== null ? `访问量：${visitCount}` : '访问量：加载中...'}
-    </p>
-    <p>Copyright © 2021 - 2025 Linfong</p>
 </div>
   );
 };
