cc5ed6d4c39554de73b0251fb0a6a290d1211cdc
hxlong2024 <hxlong@zufe.edu.cn>
2025-02-24 15:44:18 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -19,22 +19,18 @@ const Footer = () => {
 
   return (
     <div className='text-xs whitespace-nowrap text-purple-900 dark:text-rose-200 py-4'>
-      <a 
-        href='https://www.notion.so/1a4ee4c653c080beb580f0f0bb905be3?v=1a4ee4c653c08147b7b0000c9784bf6b' 
-        target='_blank' 
-        rel='noopener noreferrer' 
-        className='block mb-2 underline'
-      >
+<div class="text-xs whitespace-nowrap text-purple-900 dark:text-rose-200 py-4">
+    <a href="https://www.notion.so/1a4ee4c653c080beb580f0fobb905be3?v=1a4ee4c653c08147b7b0000c9784bf6b" target="_blank" rel="noopener noreferrer" class="block mb-2 underline">
         增加网页
-      </a>
-      <a 
-        href='https://github.com/hxlong2024/IndWebIndex-1/blob/main/README.md' 
-        target='_blank' 
-        rel='noopener noreferrer' 
-        className='block mb-2 underline'
-      >
+    </a>
+    <a href="https://github.com/hxlong2024/IndWebIndex-1/blob/main/README.md" target="_blank" rel="noopener noreferrer" class="block mb-2 underline">
         获取更新
-      </a>
+    </a>
+    </div>
+
+
+    
+    <div className='text-xs whitespace-nowrap text-purple-900 dark:text-rose-200 py-4'>
       <p className='mb-1'>
         {visitCount !== null ? `访问量：${visitCount}` : '访问量：加载中...'}
       </p>
