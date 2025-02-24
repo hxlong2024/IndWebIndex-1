697d827e9e9ebcf91a82a283dbb0cb4c3a03ae45
hxlong2024 <hxlong@zufe.edu.cn>
2025-02-24 15:01:27 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -20,17 +20,17 @@ const Footer = () => {
   return (
     <div className='text-xs whitespace-nowrap text-purple-900 dark:text-rose-200 py-4'>
       <a 
-        href='https://nowscott.notion.site/134f941cf9b880e1b00ee5bdf55fd71d?pvs=105' 
+        href='https://www.notion.so/1a4ee4c653c080beb580f0f0bb905be3?v=1a4ee4c653c08147b7b0000c9784bf6b' 
         target='_blank' 
         rel='noopener noreferrer' 
         className='block mb-2 underline'
       >
-        投稿网页
+        增加网页
       </a>
       <p className='mb-1'>
         {visitCount !== null ? `访问量：${visitCount}` : '访问量：加载中...'}
       </p>
-      <p>Copyright © 2021 - NowScott</p>
+      <p>Copyright © 2021 - 2025 Linlong</p>
     </div>
   );
 };
